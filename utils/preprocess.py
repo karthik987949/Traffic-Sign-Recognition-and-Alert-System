eb6af2c223d3b7aaf0b0e90fa0fb2806e7c4352e
karthik987949 <saikarthik9392@gmail.com>
2025-08-22 22:16:25 +0530

    Update preprocess.py

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,1 +1,8 @@
+import cv2
 
+def draw_bounding_box(frame, box, label, confidence):
+    x1, y1, x2, y2 = map(int, box)
+    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    label_text = f"{label} ({confidence:.2f})"
+    cv2.putText(frame, label_text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+    return frame
